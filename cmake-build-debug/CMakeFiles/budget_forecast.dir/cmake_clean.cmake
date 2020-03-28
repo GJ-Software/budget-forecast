f3777d54ab6f3eaed14acdb01df3497e2e5104ee
Ivan Buldakov <iv081198@yandex.ru>
2020-03-28 19:16:27 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/budget_forecast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/budget_forecast.dir/main.cpp.o"
+  "budget_forecast.pdb"
+  "budget_forecast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/budget_forecast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
