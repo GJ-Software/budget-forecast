6d883adb5452d93c73b5b7e698610aba27428fb4
Jyrs <gj-krsnv@ya.ru>
2020-04-26 00:22:28 +0300

    include SQLiteCpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
+#include <sqlite3.h>
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <gtkmm.h>
 #include <iostream>
-#include  <gtkmm.h>
+
+#ifdef SQLITECPP_ENABLE_ASSERT_HANDLER
+namespace SQLite
+{
+    void assertion_failed(const char* apFile, const long apLine, const char* apFunc, const char* apExpr, const char* apMsg)
+    {
+        // Print a message to the standard error output stream, and abort the program.
+        std::cerr << apFile << ":" << apLine << ":" << " error: assertion failed (" << apExpr << ") in " << apFunc << "() with message \"" << apMsg << "\"\n";
+        std::abort();
+    }
+}
+#endif
+
 
 Gtk::Window* mainWindow = nullptr;
 
@@ -9,6 +24,24 @@ static void testClicked(Gtk::Label* heckLabel) {
 }
 
 int main (int argc, char *argv[]) {
+
+    try {
+        SQLite::Database db("/assets/data/Test_DB.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+        std::cout << "SQLite database file '" << db.getFilename().c_str() << "' opened successfully\n";
+
+        int nb = db.exec("INSERT INTO test VALUES (NULL, \"test\")");
+        std::cout << "INSERT INTO test VALUES (NULL, \"test\")\", returned " << nb << std::endl;
+
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "SQLite exception: " << e.what() << std::endl;
+        return EXIT_FAILURE; // unexpected error : exit the example program
+    }
+    remove("Test_DB.db3");
+
+
+
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.budget-forecast");
     auto refBuilder = Gtk::Builder::create();
     try {
